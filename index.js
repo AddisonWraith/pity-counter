88ed264d4c4fa48b1109100083b29795a57ed1b0
AddisonWraith <142698868+AddisonWraith@users.noreply.github.com>
2023-10-25 16:04:30 -0700

    added local project to github
    simple counter application. 1 button increases count by one, another button increases count by 10. third button prints current count to screen and resets counter to 0

---
+++ index.js
@@ -0,0 +1,25 @@
+// grab the count-el element, store it in a countEl variable
+
+let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+
+    // set countEl's innerText to the count
+}
+
+function incrementByTen() {
+    count += 10
+    countEl.innerText = count
+}
+
+
+function save() {
+    let saveMsg = count + " wishes - "
+    saveEl.textContent += saveMsg
+    count = 0
+    countEl.innerText = count
+}
